257f10e988ef3952cecfab218f5581df5c2b6312
anischenkozhenya <43790223+anischenkozhenya@users.noreply.github.com>
2019-08-31 17:50:14 +0300

    +Task2

--- TPL/Task1/Program.cs
+++ TPL/Task1/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+//Создайте массив чисел размерностью в 1 000 000 или более.
+//Используя генератор случайных чисел, проинициализируйте 
+//этот массив значениями. Создайте PLINQ запрос, который 
+//позволит получить все нечетные числа из исходного массива.
 
 namespace Task1
 {
@@ -10,6 +12,30 @@ namespace Task1
     {
         static void Main(string[] args)
         {
+            Random random = new Random();
+            int[] massiv = new int[1000000];
+            for (int i = 0; i < massiv.Length; i++)
+            {
+                massiv[i] = random.Next(0, 1000);
+            }
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            int[] ms = massiv.AsParallel().Where((i) => i % 2 != 0).Select((i) => i).ToArray();
+            //var mass = from item in massiv.AsParallel()
+            //           where item % 2 != 0
+            //           select item;
+            //int[] ms = mass.ToArray();
+            stopwatch.Stop();
+
+            for (int i = 0; i < ms.Length; i++)
+            {
+                Console.WriteLine(ms[i]);
+
+            }
+            Console.WriteLine("Время получения только нечетных чисел: " + stopwatch.Elapsed.TotalSeconds);
+            Console.WriteLine("Для выхода нажмите любую кнопку...");
+            Console.ReadKey();
         }
     }
 }
---
+++ TPL/Task2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+//Создайте два метода, которые будут выполняться в рамках параллельных задач.
+//Организуйте вызов этих методов при помощи Invoke таким образом, чтобы основной 
+//поток программы(метод Main) не остановил свое выполнение.
+namespace Task2
+{
+    class Program
+    {
+        /// <summary>
+        /// 5 секунд деятельности
+        /// </summary>
+        static void Method1()
+        {
+            Console.WriteLine("Method1 start");
+            Thread.Sleep(5000);
+            Console.WriteLine("Method1 end");
+        }
+        /// <summary>
+        /// 8 секунд деятельности
+        /// </summary>
+        static void Method2()
+        {
+            Console.WriteLine("Method2 start");
+            Thread.Sleep(8000);
+            Console.WriteLine("Method2 end");
+        }
+        /// <summary>
+        /// 4 секунд деятельности
+        /// </summary>
+        static void Method3(Task task)
+        {
+            Console.WriteLine("Method3 start");
+            Thread.Sleep(4000);
+            Console.WriteLine("Method3 end");
+        }
+
+        static void Main(string[] args)
+        {
+            //1 способ
+            Task task1 = new Task(new Action(Method1));
+            Task task2 = new Task(Method2);
+            task1.Start();
+            task2.Start();
+            task1.Wait();
+            task2.Wait();
+            Console.WriteLine(new string('_', 25));
+            //2 способ
+            Task task = Task.Factory.StartNew(() => Parallel.Invoke(Method2, Method1));
+            task.Wait();
+            Console.WriteLine(new string('_', 25));
+            //3 способ
+            task1 = new Task(new Action(Method1));
+            Task task3 = task1.ContinueWith(Method3);
+            task1.Start();
+            Task.WaitAll(task3, task1);
+
+            Console.WriteLine("Для выхода нажмите любую клавишу...");
+            Console.ReadKey();
+        }
+    }
+}
+
